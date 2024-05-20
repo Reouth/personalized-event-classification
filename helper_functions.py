82222c82aeb5ef087da420083cb0d0747ddcb5ba
Reouth <74666037+Reouth@users.noreply.github.com>
2024-05-20 22:57:58 +0300

    Add files via upload

--- helper_functions.py
+++ helper_functions.py
@@ -1,25 +1,26 @@
 import os
 import pandas as pd
-def list_folders_in_directory(directory_path):
+def list_csv_files_in_directory(directory_path):
     try:
         # List all items in the directory
         items = os.listdir(directory_path)
 
-        # Filter out only the folders
-        folders = [item for item in items if os.path.isdir(os.path.join(directory_path, item))]
+        # Filter out only the CSV files
+        csv_files = [item for item in items if item.endswith('.csv') and os.path.isfile(os.path.join(directory_path, item))]
 
-        return folders
+        return csv_files
     except FileNotFoundError:
         print(f"The directory {directory_path} does not exist.")
         return []
 
 def csv_checkpoint(csv_folder,cls,test_image):
     image_flag =False
-    cls_list = list_folders_in_directory(csv_folder)
+    csv_list = list_csv_files_in_directory(csv_folder)
     filepath = "{}_results.csv".format(cls)
     csv_path = os.path.join(csv_folder, filepath)
-    if csv_path in cls_list:
+    if csv_path in csv_list:
         df_sd = pd.read_csv(csv_path)
+        print(df_sd)
         if test_image in df_sd['GT image name'].unique():
             image_flag = True
     else:
