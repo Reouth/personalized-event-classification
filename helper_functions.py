e9dd952e83f19cfbe5156ddcd3257f45ff6c38d6
Reouth <74666037+Reouth@users.noreply.github.com>
2024-05-20 16:35:57 +0300

    Add files via upload

---
+++ helper_functions.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+def list_folders_in_directory(directory_path):
+    try:
+        # List all items in the directory
+        items = os.listdir(directory_path)
+
+        # Filter out only the folders
+        folders = [item for item in items if os.path.isdir(os.path.join(directory_path, item))]
+
+        return folders
+    except FileNotFoundError:
+        print(f"The directory {directory_path} does not exist.")
+        return []
+
+def csv_checkpoint(csv_folder,cls,test_image, pipline= ""):
+    image_flag =False
+    cls_list = list_folders_in_directory(csv_folder)
+
+    filepath = "{}**{}_results.csv".format(pipline,cls)
+    csv_path = os.join.path(csv_folder, filepath)
+    os.makedirs(csv_path, exist_ok=True)
+    if csv_path in cls_list:
+        df_sd = pd.read_csv(csv_path)
+        if test_image in df_sd['GT image name'].unique():
+            image_flag = True
+    else:
+        df_sd = pd.DataFrame()
+    return image_flag, df_sd, csv_path
