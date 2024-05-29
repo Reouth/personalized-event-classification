b6315f2be64faac76b42b7ce17115bf82eb688f0
Reouth <74666037+Reouth@users.noreply.github.com>
2024-05-29 15:38:13 +0300

    Add files via upload

--- similarity_calc.py
+++ similarity_calc.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def calculate_top_k_accuracy(csv_path, correct_class_col, pred_class_col, loss_col, k):
+def calculate_top_k_accuracy(csv_path, correct_class_col, pred_class_col, loss_col, k,down):
     df = pd.read_csv(csv_path)
     correct, counts = 0, 0
     group_accuracies = {}
@@ -11,7 +11,7 @@ def calculate_top_k_accuracy(csv_path, correct_class_col, pred_class_col, loss_c
     # Iterate over each group
     for image_id, group in grouped:
 
-        group = group.sort_values(by=loss_col, ascending=True)
+        group = group.sort_values(by=loss_col, ascending=down)
         top_k_predictions = group[pred_class_col].head(k).tolist()
         correct_class = group[correct_class_col].iloc[0]
         if correct_class in top_k_predictions:
