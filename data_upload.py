7f8208a9e8d5ae7405af90dc801582062f203f25
Reouth <74666037+Reouth@users.noreply.github.com>
2024-05-29 10:14:33 +0300

    Add files via upload

--- data_upload.py
+++ data_upload.py
@@ -19,13 +19,12 @@ def is_image(file_path):
         return False
 
 
-def upload_images(base_path,class_batch=float('inf'),max_frames = float('inf')):
+def upload_images(base_path, class_batch=float('inf'), max_frames=float('inf')):
     """Recursively load images with their new names into a list."""
     image_data = []  # List to hold image data along with their names
+    image_counts = {}  # Dictionary to store image counts for each directory
+    total_frames_count = 0  # Total frame count across all directories
 
-    # Dictionary to store image counts for each directory
-    image_counts = {}
-    total_frames_count = 0
     # Iterate through items in the base directory
     for item in os.listdir(base_path):
         item_path = os.path.join(base_path, item)
@@ -33,26 +32,26 @@ def upload_images(base_path,class_batch=float('inf'),max_frames = float('inf')):
         if os.path.isdir(item_path):
             print(f"Entering directory: {item_path}")
             # Recursively load images from subdirectories
-            image_data += upload_images(item_path)
+            image_data += upload_images(item_path, class_batch, max_frames)
         elif is_image(item_path):
-            # Get the folder name
-            folder_name = os.path.basename(base_path)
+            folder_name = os.path.basename(base_path)  # Get the folder name
 
-            # Initialize or increment the image count for this folder
             if folder_name not in image_counts:
                 image_counts[folder_name] = 0
-            if total_frames_count> max_frames:
+
+            # Check if the total frames limit or class batch limit is reached
+            if total_frames_count >= max_frames or image_counts[folder_name] >= class_batch:
                 return image_data
-            if image_counts[folder_name]>class_batch:
-                break
+
             image_counts[folder_name] += 1
+            total_frames_count += 1
 
             # Construct the new name for the image
             frame_number = image_counts[folder_name]
             new_name = f"{folder_name}_{frame_number:03d}.jpg"  # Zero-padded frame number
 
             # Append image data to the list
-            image_data.append((new_name, Image.open(item_path),item_path))
+            image_data.append((new_name, Image.open(item_path), item_path))
         else:
             print(f"Not an image file: {item_path}")
 
